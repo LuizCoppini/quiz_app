2e9148e67ae181aaa4601e29c3b84dfdab5ad130
Luiz-Coppini <luiz.coppini@hootzpa.com.br>
2025-03-14 12:25:24 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/background_app.jpg')}
+      style={styles.background}
+      resizeMode="cover" // Ajusta a imagem ao tamanho da tela
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white', // Para garantir visibilidade na imagem
   },
 });
